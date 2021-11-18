3c961528f2a29c00043a6b989ec16369a960bc84
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-11-18 02:02:57 -0500

    implement pool, client and related methods, as well as test transaction

--- db/index.js
+++ db/index.js
@@ -4,5 +4,36 @@ const config = require('./config');
 const pool = new Pool(config);
 
 module.exports = {
-  query: (text, params, callback) => pool.query(text, params, callback),
+  query: async (text, params) => {
+    const start = Date.now();
+    const res = await pool.query(text, params);
+    const duration = Date.now() - start;
+    console.log('executed query', { text, duration, rows: res.rowCount });
+    return res;
+  },
+  getClient: async () => {
+    const client = await pool.connect();
+    const { query } = client;
+    const { release } = client;
+    // set a timeout of 5 seconds, after which we will log this client's last query
+    const timeout = setTimeout(() => {
+      console.error('A client has been checked out for more than 5 seconds!');
+      console.error(
+        `The last executed query on this client was: ${client.lastQuery}`
+      );
+    }, 5000);
+    client.query = (...args) => {
+      client.lastQuery = args;
+      return query.apply(client, args);
+    };
+    client.release = () => {
+      // clear our timeout
+      clearTimeout(timeout);
+      // set the methods back to their old un-monkey-patched version
+      client.query = query;
+      client.release = release;
+      return release.apply(client);
+    };
+    return client;
+  },
 };
--- routes/questions.js
+++ routes/questions.js
@@ -3,13 +3,26 @@ const db = require('../db');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  db.query('SELECT * from questions', [], (err, result) => {
-    if (err) {
-      return next(err);
-    }
-    res.send(result.rows[0]);
-  });
+router.get('/', async (req, res, next) => {
+  const client = await db.getClient();
+  let response;
+  try {
+    await client.query('BEGIN');
+    const queryText =
+      'INSERT INTO questions(body, asker_name, user_id) VALUES ($1, $2, $3) RETURNING id';
+    response = await client.query(queryText, ['review2', 'bindman', 1]);
+    const insertAnswerText =
+      'INSERT INTO answers(question_id, body, answerer_name, user_id) VALUES ($1, $2, $3, $4) RETURNING id';
+    const insertAnswerValues = [response.rows[0].id, 'answer2', 'ted', 1];
+    response = await client.query(insertAnswerText, insertAnswerValues);
+    await client.query('COMMIT');
+  } catch (e) {
+    await client.query('ROLLBACK');
+    throw e;
+  } finally {
+    client.release();
+    res.status(201).send();
+  }
 });
 
 module.exports = router;
