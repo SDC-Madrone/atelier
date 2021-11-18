d30f2d1a0f64e694b6cfd19a5b03be67f14653b2
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-11-18 15:09:47 -0500

    revert COPY to psql \copy

--- data/data-loader.sql
+++ data/data-loader.sql
@@ -1,10 +1,8 @@
 \c q_and_a;
 
-COPY questions(id, product_id, body, date_written, asker_name, asker_email, reported, helpful)
-From '/Users/adamklingbaum/Documents/coding/hack_reactor/sdc/sdc-madrone-api-qa/data/raw-data/questions.csv'
-DELIMITER ','
-csv header;
+\copy questions(id, product_id, body, date_written, asker_name, asker_email, reported, helpful) from '/Users/adamklingbaum/Documents/coding/hack_reactor/sdc/sdc-madrone-api-qa/data/raw-data/questions.csv' delimiter ',' csv header;
+
+\copy answers(id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful) from '/Users/adamklingbaum/Documents/coding/hack_reactor/sdc/sdc-madrone-api-qa/data/raw-data/answers.csv' delimiter ',' csv header;
 
 
-
-
+\copy photos(id, answer_id, url) from '/Users/adamklingbaum/Documents/coding/hack_reactor/sdc/sdc-madrone-api-qa/data/raw-data/answers_photos.csv' delimiter ',' csv header;
