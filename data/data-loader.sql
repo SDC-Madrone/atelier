526d7f63774e8c2eaf396f3d485b44d61550e225
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-11-18 15:16:13 -0500

    remove whitespace

--- data/data-loader.sql
+++ data/data-loader.sql
@@ -4,5 +4,4 @@
 
 \copy answers(id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful) from '/Users/adamklingbaum/Documents/coding/hack_reactor/sdc/sdc-madrone-api-qa/data/raw-data/answers.csv' delimiter ',' csv header;
 
-
 \copy photos(id, answer_id, url) from '/Users/adamklingbaum/Documents/coding/hack_reactor/sdc/sdc-madrone-api-qa/data/raw-data/answers_photos.csv' delimiter ',' csv header;
