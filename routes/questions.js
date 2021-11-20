29fe454dcce08cd1167be63919058b30652d4ecf
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-11-19 20:44:47 -0500

    implement answers by questionId route

--- routes/questions.js
+++ routes/questions.js
@@ -45,6 +45,35 @@ router.get('/', async (req, res, next) => {
     .catch(() => res.status(400).send());
 });
 
+router.get('/:question_id/answers', async (req, res, next) => {
+  const { question_id: questionId } = req.params;
+  const { page, count } = req.query;
+  const queryText = `
+      SELECT
+        answers.id AS answer_id,
+        answers.body AS body,
+        to_timestamp(answers.date_written/1000) AS date,
+        answers.answerer_name AS answered_name,
+        answers.helpful AS helpfulness,
+        array(SELECT url FROM photos WHERE photos.answer_id=answers.id) as photos
+      FROM answers WHERE question_id=$1 AND reported=false
+      ORDER BY answers.date_written DESC
+      LIMIT $2
+      OFFSET (($3 - 1) * $2)`;
+  const queryValues = [questionId, count, page];
+  await db
+    .query(queryText, queryValues)
+    .then((response) => {
+      res.status(200).json({
+        question: questionId,
+        page,
+        count,
+        results: response.rows,
+      });
+    })
+    .catch(() => res.status(400).send());
+});
+
 router.put('/:question_id/helpful', (req, res, next) => {
   const { question_id: questionId } = req.params;
   const queryText = `
