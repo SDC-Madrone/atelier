1867f4eebbc6b05fcd0f310f208f2db623be5804
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-11-19 20:29:18 -0500

    move answer helpful to answers router

--- routes/answers.js
+++ routes/answers.js
@@ -3,13 +3,18 @@ const db = require('../db');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  db.query('SELECT * from answers', [], (err, result) => {
-    if (err) {
-      return next(err);
-    }
-    res.send(result.rows[0]);
-  });
+router.put('/:answer_id/helpful', (req, res, next) => {
+  const { answer_id: answerId } = req.params;
+  const queryText = `
+    UPDATE answers
+    SET helpful = helpful + 1
+    WHERE id=$1`;
+  const queryValues = [answerId];
+  db.query(queryText, queryValues)
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch(() => res.status(400).send());
 });
 
 module.exports = router;
--- routes/questions.js
+++ routes/questions.js
@@ -58,17 +58,5 @@ router.put('/:question_id/helpful', (req, res, next) => {
     })
     .catch(() => res.status(400).send());
 });
-router.put('/:answer_id/helpful', (req, res, next) => {
-  const { answer_id: answerId } = req.params;
-  const queryText = `
-    UPDATE answers
-    SET helpful = helpful + 1
-    WHERE id=$1`;
-  const queryValues = [answerId];
-  db.query(queryText, queryValues)
-    .then(() => {
-      res.status(204).send();
-    })
-    .catch(() => res.status(400).send());
-});
+
 module.exports = router;
